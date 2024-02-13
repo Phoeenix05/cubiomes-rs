945d924501187b45502fbb4e72a06a405c8eba2a
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-13 15:26:06 +0200

    initial commit

---
+++ src/cubiomes.rs
@@ -0,0 +1,16 @@
+// #![allow(
+//     dead_code,
+//     non_snake_case,
+//     non_camel_case_types,
+//     non_upper_case_globals
+// )]
+
+pub mod biomenoise;
+pub mod biomes;
+pub mod finders;
+pub mod generator;
+pub mod layers;
+pub mod noise;
+pub mod quadbase;
+pub mod rng;
+pub mod util;
