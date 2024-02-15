ca9425327c2e6431a96a0e7159ce90eb0f06e16f
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-15 09:03:52 +0200

    chore: updated build script

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
     println!("cargo:rustc-link-search=native=./cubiomes");
     println!("cargo:rustc-link-lib=static=cubiomes");
+
+    std::env::set_current_dir("./cubiomes").expect("failed to change directory");
+    std::process::Command::new("make")
+        .output()
+        .expect("failed to run `make`");
 }
